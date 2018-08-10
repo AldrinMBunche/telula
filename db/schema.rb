77d294dc9cd8bb8d53212068c843f406f026c95c
lms4u <bunchealdrin@gmail.com>
2018-08-10 09:27:46 +0200

    Enviroment setup on old version VM

--- db/schema.rb
+++ db/schema.rb
@@ -41,42 +41,6 @@ ActiveRecord::Schema.define(version: 20180807120826) do
     t.datetime "updated_at",              null: false
   end
 
-  create_table "clients", force: :cascade do |t|
-    t.integer  "client_id"
-    t.integer  "company_id"
-    t.string   "type"
-    t.string   "name"
-    t.string   "business_reg_no"
-    t.integer  "tel_no"
-    t.integer  "fax_no"
-    t.string   "account_no"
-    t.string   "r_address_line_1"
-    t.string   "r_address_line_2"
-    t.string   "r_suburb"
-    t.string   "r_city"
-    t.string   "r_province"
-    t.integer  "r_postal_code"
-    t.string   "r_country"
-    t.string   "p_address_line_1"
-    t.string   "p_address_line_2"
-    t.string   "p_suburb"
-    t.string   "p_city"
-    t.string   "p_province"
-    t.integer  "p_postal_code"
-    t.string   "p_country"
-    t.integer  "client_branch"
-    t.string   "sales_person"
-    t.string   "branch_id"
-    t.string   "sdl_number"
-    t.string   "contact_name"
-    t.string   "main_email"
-    t.string   "wdyhau"
-    t.integer  "credit_limit"
-    t.string   "payment_term"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
-  end
-
   create_table "learner_events", force: :cascade do |t|
     t.integer  "TRAINING_ID"
     t.integer  "LEARNER_ID"
@@ -175,16 +139,6 @@ ActiveRecord::Schema.define(version: 20180807120826) do
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
   end
-
-  create_table "pg_search_documents", force: :cascade do |t|
-    t.text     "content"
-    t.integer  "searchable_id"
-    t.string   "searchable_type"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-  end
-
-  add_index "pg_search_documents", ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable_type_and_searchable_id", using: :btree
 
   create_table "qualifications", force: :cascade do |t|
     t.integer  "qualification_id"
